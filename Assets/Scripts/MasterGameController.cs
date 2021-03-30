9e24f7d08da62a3899fd2bcb9075f56e903467a1
Cormick <cvhnilicka@wisc.edu>
2021-03-29 19:53:35 -0500

    deck builder and shuffle done for environment deck

--- Assets/Scripts/MasterGameController.cs
+++ Assets/Scripts/MasterGameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static GameInformation;
 
 public class MasterGameController : MonoBehaviour
 {
@@ -16,6 +17,10 @@ public class MasterGameController : MonoBehaviour
     float randomTimer;
     [SerializeField] float rotateTimer = 5f;
 
+
+    // Deck Info
+    ENVIRONMENT_CARD_TYPES[] currentEnvironmentDeck;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +28,10 @@ public class MasterGameController : MonoBehaviour
         currentPlayState = PLAY_STATE.WAITING;
         myBoard = GetComponentInChildren<BoardController>();
         DisplayGameStates();
-
+        currentEnvironmentDeck = ShuffleDeck();
+        PrintDecks();
+        if (!DeckChecker()) print("Something wrong with deck");
+        else print("Deck Is Proper");
     }
 
     void Timers()
@@ -101,5 +109,75 @@ public class MasterGameController : MonoBehaviour
     {
         print("Current Game State: " + currentGameState);
         print("Current Play State: " + currentPlayState);
+    }
+
+    void PrintDecks()
+    {
+        print("Environment Deck:");
+        for (int i = 0; i < currentEnvironmentDeck.Length; i++)
+        {
+            print(i + " : " + currentEnvironmentDeck[i]);
+        }
+    }
+
+
+
+    ENVIRONMENT_CARD_TYPES[] ShuffleDeck()
+    {
+        ENVIRONMENT_CARD_TYPES[] ret = UNSHUFFLED_DECK;
+        //for (int i = currentEnvironmentDeck.Length-1; i > 0; i--)
+        //{
+        //    int r = Random.Range(0, i);
+        //    currentEnvironmentDeck[r] = UNSHUFFLED_DECK[i];
+        //}
+
+        for(int i = 0; i < ret.Length; i++)
+        {
+            int r = Random.Range(i, ret.Length);
+            ENVIRONMENT_CARD_TYPES t = ret[r];
+            ret[r] = ret[i];
+            ret[i] = t;
+        }
+        return ret;
+    }
+
+
+    bool DeckChecker()
+    {
+        bool ret = true;
+        int sfo = 0,
+            sfe = 0,
+            sfa = 0,
+            wo = 0,
+            wd = 0,
+            cs = 0;
+        for (int i = 0; i < currentEnvironmentDeck.Length; i++)
+        {
+            switch (currentEnvironmentDeck[i])
+            {
+                case ENVIRONMENT_CARD_TYPES.COLDSNAP: cs += 1;
+                    break;
+                case ENVIRONMENT_CARD_TYPES.SNOWFALLALL: sfa += 1;
+                    break;
+                case ENVIRONMENT_CARD_TYPES.SNOWFALLODD: sfo += 1;
+                    break;
+                case ENVIRONMENT_CARD_TYPES.SNOWFALLEVEN: sfe += 1;
+                    break;
+                case ENVIRONMENT_CARD_TYPES.WARMDAY: wd += 1;
+                    break;
+                case ENVIRONMENT_CARD_TYPES.WHITEOUT: wo += 1;
+                    break;
+            }
+        }
+        if (sfo > NUM_SNOWFALLODD ||
+            sfe > NUM_SNOWFALLEVEN ||
+            sfa > NUM_SNOWFALLALL ||
+            wo > NUM_WHITEOUT ||
+            wd > NUM_WARMDAY ||
+            cs > NUM_COLDSNAP)
+        {
+            ret = false;
+        }
+        return ret;
     }
 }
