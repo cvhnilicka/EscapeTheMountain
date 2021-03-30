5c2b70f9cade2a51b1955f6d7f9a015dacbe7492
Cormick <cvhnilicka@wisc.edu>
2021-03-30 14:42:25 -0500

    very beginnings of adding in an adventurer

---
+++ Assets/Scripts/AdventurerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdventurerController : MonoBehaviour
+{
+
+
+    /*
+     * Single adventurer specific functionality 
+     *
+     * Set Type
+     * Get Type
+     *
+     * Set Health
+     * Get Health
+     * Take Damage
+     * Add Health
+     * Get CurrHealth
+     * 
+     * Set CarryLimit
+     * Get CarryLimit
+     * Add Weight
+     * Remove Weight
+     * Get CurrCarry
+     * 
+     * 
+     * [] Carried Gear
+     * [] Carried Equipment
+     * 
+     * 
+     * Avialable actions?
+     * 
+     * etc
+     * 
+     * 
+     * 
+     * 
+     * **/
+
+
+
+
+    /*
+     * Looks like i can set the location using the snow controllers on each tile
+     * **/
+    public SnowController test;
+
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.position = new Vector2(test.gameObject.transform.position.x, test.gameObject.transform.position.y);
+        //transform.rotation = test.gameObject.transform.rotation;
+
+        print("AN ADVENTURERS APPEARS");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/AdventurersController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdventurersController : MonoBehaviour
+{
+
+    /*
+     * This can start to serve as the master controller for the adventurers
+     * 
+     * Initiallizing them
+     * 
+     * keeping track of whose turn it is
+     * 
+     * check for deaths
+     * 
+     * check for instants
+     * 
+     * maybe store adventurer difference info? i.e. snow patrol extra turn?
+     * 
+     * 
+     * **/
+
+    public GameObject adventurerPrefab;
+    public SnowController startTileTEST;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeCook();
+    }
+
+    void InitializeCook()
+    {
+        GameObject cook = Instantiate(adventurerPrefab, new Vector2(startTileTEST.transform.position.x, startTileTEST.transform.position.y), Quaternion.identity);
+        cook.GetComponent<AdventurerController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
